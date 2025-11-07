c2f685e2337df5e3f66872bee12332f911cfcf17
zrrgz <vorameet263@gmail.com>
2025-11-07 17:40:21 +0530

    Exercise 1.1 Done

---
+++ Part1/courseinfo/courseinfo/src/App.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header data={course} />
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+    </div>
+  )
+}
+const Header = (props) =>{
+  return (
+  <h1>{props.data}</h1>
+  )
+}
+const Content= (props)=>{
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+const Total=(props)=>{
+  return (
+    <p>Number of exercises {props.e1+props.e2+props.e3}</p>
+  )
+}
+
+export default App
